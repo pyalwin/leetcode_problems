b9a251a7911f28ce480b0c4244c44cf671679202
pythalwin <arun.tgode@gmail.com>
2022-03-12 19:32:46 +0000

    Rearranging the structure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# from max_sum_subarray import *
-# from smallest_sub_array_given_sum import *
-# from fruits_in_the_baskets import *
-from longest_substring_with_distinct_char import *
+# Sliding window approach based problems
+# from sliding_window.max_sum_subarray import *
+# from sliding_window.smallest_sub_array_given_sum import *
+# from sliding_window.fruits_in_the_baskets import *
+# from sliding_window.longest_substring_with_distinct_char import *
